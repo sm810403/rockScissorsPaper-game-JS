e59c1570ac0d03b722d123d92aa0cbc18f3b4b37
JaeIn <sm810403@hotmail.com>
2020-06-16 20:00:45 -0700

    three commit
    struggling.. clearInterval applied

--- index.js
+++ index.js
@@ -2,63 +2,76 @@ let rockBtn = document.querySelector('.btn-1');
 let sciBtn = document.querySelector('.btn-2');
 let papBtn = document.querySelector('.btn-3');
 let btns = document.querySelectorAll('.btn');
-let img = document.querySelector('.img');
-let img2 = document.querySelector('.img2');
+let imgFirst = document.querySelector('.img-first');
+let imgSecond = document.querySelector('.img-second');
 let restart = document.querySelector('.restart');
 let result = document.querySelector('.result');
 let count = 0;
 let relap;
-restart.addEventListener('click', reSet);
-function reSet(){
-   relap = setInterval(()=>{
-    count++;
-    img.style.backgroundImage = `url(images/game-${count}.svg)`
-    img2.style.backgroundImage = `url(images/game-${count}.svg)`
-    if ( count === 3) {
-        count = 0;
-        }  
 
-    }, 500)
-}
+// reset after finishing match
+// restart.addEventListener('click', reSet);
+// function reSet(){
+//    relap = setInterval(()=>{
+//     count++;
+//     // imgFirst.style.backgroundImage = `url(images/game-${count}.svg)`
+//     imgSecond.style.backgroundImage = `url(images/game-${count}.svg)`
+//     if ( count === 3) {
+//         count = 0;
+//         }  
 
-var autoRun = setInterval(setDefault, 500);
+//     }, 500)
+// }
+
+// // default case
+var autoRun = setInterval(setDefault, 50);
 function setDefault() {
     count++;
-    img.style.backgroundImage = `url(images/game-${count}.svg)`
-    img2.style.backgroundImage = `url(images/game-${count}.svg)`
+    // imgFirst.style.backgroundImage = `url(images/game-${count}.svg)`
+    imgSecond.style.backgroundImage = `url(images/game-${count}.svg)`
     if ( count === 3) {
         count = 0;
     }
 }
 
 
-
+// user pick
 rockBtn.addEventListener('click', setPick1);
 
 function setPick1() {
-    autoRun = clearInterval()
-    img.style.backgroundImage = `url(images/game-2.svg)`
+    clearInterval(autoRun);
+    imgFirst.style.backgroundImage = `url(images/game-2.svg)`
+    if (random === 2) {
+        result.innerHTML = 'You won!!';
+    }
 }
 sciBtn.addEventListener('click', setPick2);
 
 function setPick2() {
-    clearInterval(autoRun, 2000);
-    img.style.backgroundImage = `url(images/game-1.svg)`
-}
+    clearInterval(autoRun);
+    imgFirst.style.backgroundImage = `url(images/game-1.svg)`
+    }
+
 papBtn.addEventListener('click', setPick3);
 
 function setPick3() {
-    clearInterval(autoRun, 2000)
-    img.style.backgroundImage = `url(images/game-3.svg)`
-}
-window.addEventListener('click', compPick);
-function compPick(){
-    var random = Math.floor(Math.random()*3)+1;
-    img2.style.backgroundImage = `url(images/game-${random}.svg)`
+    clearInterval(autoRun)
+    imgFirst.style.backgroundImage = `url(images/game-3.svg)`
 }
 
-function getResult(){
-    if (img === img2){
-        result.innerHTML = 'draw-yo!';
-    }
+
+//computer pick
+let random;
+function setCompPick(){
+    let random = Math.floor(Math.random()*3);
+    imgSecond.style.backgroundImages = `url(images/game-${random}.svg)`;
 }
+
+
+
+//result
+function getResult(){
+    
+}
+
+
